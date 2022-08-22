a9ac07e73038ba47c52be2a0c42b3736df6a82df
Matheus Gobetti <matheus.gobetti@eveclass.com>
2022-08-22 17:22:12 -0300

    First commit

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="header">
+    Header
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
+
+<style>
+.header {
+  background: #c1c1c1;
+  color: #fff;
+}
+</style>
+
+
